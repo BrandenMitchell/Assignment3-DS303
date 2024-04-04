868dce9bbf5624a4a482842008aef4f128cc86e3
BrandenMitchell <71188053+BrandenMitchell@users.noreply.github.com>
2024-04-04 14:21:28 -0500

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,23 @@
+#ifndef ASSIGNMENT3_QUEUEIMPLEMENTATION_FUNCTIONS_H
+#define ASSIGNMENT3_QUEUEIMPLEMENTATION_FUNCTIONS_H
+
+#endif //ASSIGNMENT3_QUEUEIMPLEMENTATION_FUNCTIONS_H
+
+
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int linear_search(vector<int>& items, int& target, size_t pos_first, int& instance );
+
+int linear_search_wrapper(vector<int>& items,int& target); //we will call wrapper which will call the func
+
+
+
+
+
+
